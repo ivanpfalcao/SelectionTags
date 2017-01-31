aec7a61c6574e0b4a23ae9a3a58bef8187bcae6c
ivanpfalcao <ivanpfalcao@hotmail.com>
2017-01-31 09:12:10 -0300

    V1_0
    Version 1_0 
    \o/

--- SelectionTags.js
+++ SelectionTags.js
@@ -6,6 +6,22 @@ var fieldsTranslations = [{
 var globalSelectedFields; // Global selected fields
 var app; // Global app
 
+// Set the colors to be selected
+var palette = [
+	"#b0afae",
+	"#7b7a78",
+	"#545352",
+	"#4477aa",
+	"#7db8da",
+	"#b6d7ea",
+	"#46c646",
+	"#f93f17",
+	"#ffcf02",
+	"#276e27",
+	"#ffffff",
+	"#000000"
+];
+
 define( ["qlik","jquery", "text!./src/SelectionTags.css", "text!./template.html"], function (qlik, $, cssContent, template ) {'use strict';
     $("<style>").html(cssContent).appendTo("head");
 	return {
@@ -25,16 +41,57 @@ define( ["qlik","jquery", "text!./src/SelectionTags.css", "text!./template.html"
 			component : "accordion",
 			items : {
 				settings : {
-					uses : "settings",
-					items : {
-						initFetchRows : {
-							ref : "qHyperCubeDef.qInitialDataFetch.0.qHeight",
-							label : "Initial fetch rows",
-							type : "number",
-							defaultValue : 50
+					uses : "settings"									
+				},
+				MyList: {                              
+					type: "array",                       
+					ref: "layoutList",                     
+					label: "Line Settings",    
+					itemTitleRef: "label",               
+					allowAdd: true,                      
+					allowRemove: true,                   
+					addTranslation: "Add Item",          
+					items: {                                                             
+						fieldName: {                           
+							type: "string",                  
+							ref: "fieldName",                    
+							label: "Field Name",                  
+							expression: "always",          
+							defaultValue: "Field Name"            
+						}, 
+						/*		
+						DropDownDecSep: {
+							type: "string",
+							component: "dropdown",
+							label: "Decimal Separator",
+							ref: "DecSep",
+							options: [{
+								value: "DotSep",
+								label: "Dot as Decimal Separator"
+							}, 
+							{
+								value: "CommaSep",
+								label: "Comma as Decimal Separator"
+							}],
+							defaultValue: "CommaSep"
+						},	
+						*/
+						backgroundColor: {
+							label:"Background Color",
+							component: "color-picker",
+							ref: "backgroundColor",
+							type: "integer",
+							defaultValue: 1
+						},
+						fontColor: {
+							label:"Font Color",
+							component: "color-picker",
+							ref: "fontColor",
+							type: "integer",
+							defaultValue: 10
 						}
-					}
-				}
+					}                                    
+				} 
 			}
 		},
 		snapshot : {
@@ -50,22 +107,57 @@ define( ["qlik","jquery", "text!./src/SelectionTags.css", "text!./template.html"
 			//Generate View
 			app = qlik.currApp(this);
 			var selectionState = app.selectionState;
+			
             var selections = app.getList("CurrentSelections", function(reply) {
 				globalSelectedFields = reply.qSelectionObject.qSelections;
 				var globalSelectedFieldsLength = globalSelectedFields.length,html = "";
 				html+='<div class="qstag-container">';
+				
+				/*
+				var tagDefaultBackgroundColor='#c2d6d6';
+				var tagDefaultFontColor='#000000';
+				var tagDefaultFontFamily='Arial';				
 				var tagBackgroundColor='#c2d6d6';
-				var tagFontColor='#000000';
+				var tagFontColor='#000000';				
+				var tagFontFamily='"Comic Sans MS", "Comic Sans", cursive';
+				*/
+				
+				//Get the number of items 
+				var lenItems = layout.layoutList.length;
+
+				var dictBgColor = new Array();
+				var dictFontColor = new Array();
+				for (var i=0; i<lenItems;i++){
+					var fieldName = layout.layoutList[i].fieldName;
+					var backgroundColor = layout.layoutList[i].backgroundColor;
+					var fontColor = layout.layoutList[i].fontColor;
+					dictBgColor[fieldName] = backgroundColor;
+					dictFontColor[fieldName] = fontColor;
+				}
+				
 				
 				for (var i = 0; i < globalSelectedFieldsLength; i++) {
 						
 						var selectedValues = globalSelectedFields[i].qSelected;
 						var splittedSelectedValues = selectedValues.split(", ");
+						
+						if (typeof dictBgColor[globalSelectedFields[i].qField] != 'undefined') {
+							var tagBackgroundColor=palette[dictBgColor[globalSelectedFields[i].qField]];
+							var tagFontColor=palette[dictFontColor[globalSelectedFields[i].qField]];
+							var tagFontFamily='Arial';
+						} else {
+							var tagBackgroundColor='#c2d6d6';
+							var tagFontColor='#000000';
+							var tagFontFamily='Arial';
+							
+						}
+						
+						
 						for (var j = 0; j < splittedSelectedValues.length; j++) {
 							var tagValue = globalSelectedFields[i].qField + ' : ' + splittedSelectedValues[j];
 							var tagIP = globalSelectedFields[i].qField + '||-||' + splittedSelectedValues[j]+ '||-||' + i + '||-||' + j;
 							html+='<ul class="qstag-taglist">';
-                     		html+='<li style="background-color:'+ tagBackgroundColor +';color:' + tagFontColor + ';opacity: 1;" data-tag="tag" class="qstag-tag"><div class="qstag-tag-content"><span class="qstag-tag-text">' + tagValue +'</span><a class="qstag-tag-remove" id="' + tagIP + '"></a></div></li>';
+                     		html+='<li style="background-color:'+ tagBackgroundColor +';color:' + tagFontColor + ';opacity: 1;" data-tag="tag" class="qstag-tag"><div class="qstag-tag-content"><span class="qstag-tag-text"><p style="font-family:' + tagFontFamily + ';">' + tagValue +'</span><a class="qstag-tag-remove" id="' + tagIP + '"></a></p></div></li>';
 							html+='</ul>'
 						}
                 }																
